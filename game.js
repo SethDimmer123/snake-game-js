d29e93e7889d4733619cb168a2ff8c69075e27dc
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-01 12:05:25 -0400

    initial commit

--- game.js
+++ game.js
@@ -1,18 +1,30 @@
 // render code setup
 
-let lastRenderTime = 0// default time of my last render
+let lastRenderTime = 0
 
-const SNAKE_SPEED = 2 // this will be how many times the snake moves per second
+const SNAKE_SPEED = 2 
 
- function main(currentTime) {// I WILL NOT DO ANYTHING IN MY MAIN FUNCTION UNLESS I HIT A CERTAIN THRESHOLD
+ function main(currentTime) {
 
-    window.requestAnimationFrame(main) // i always request my next animation frame no matter what.
+    window.requestAnimationFrame(main) 
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
 
-    console.log('render')// 2 times per second i get the text render printed out in console
+    console.log('render')
     lastRenderTime = currentTime 
+
+    //logic for my game 
+
+    Update() // this update loop will update all of the logic for my game.
+    //the update loop will move the snake to the correct position but not draw it 
+    // update loop will update if i ate the food or not and make the snake longer or shorter
+    //update loop will determine if i lost the game because i ran into my own tail
+    //  or ran into the edge of the screen.
+
+    draw() // the draw loop will draw everything on the screen based on my update loop.
+    // with the draw loop i will take all of my logic from update loop and say where is my snake now 
+    // lets draw the snake in the correct POSITION as well as draw the food where it needs to go 
  }
 
  window.requestAnimationFrame(main)
