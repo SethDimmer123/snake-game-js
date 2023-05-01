265a5b64c03830c0700c0ce64322b6b4b2ad0561
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-01 11:58:58 -0400

    initial commit

--- game.js
+++ game.js
@@ -6,15 +6,11 @@ const SNAKE_SPEED = 2 // this will be how many times the snake moves per second
 
     window.requestAnimationFrame(main) // i always request my next animation frame no matter what.
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    // secondsSinceLastRender lets me calculate if i actually need to move and check if
-    //if the secondsSinceLastRender is essentially LESS THAN THE TIME BETWEEN MY RENDERS
-    //which is one divided by my snake speed since my snake moves 2 times per second.
-    // 1 divided  by that would be number of seconds between each move 
-    // i have a half of a second between each move 
-    //if the time sinceLastRender is half a second  then i DO NOT NEED TO MOVE MY SNAKE 
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    console.log('render')// 2 times per second i get the text render printed out in console
     lastRenderTime = currentTime 
-    console.log(secondsSinceLastRender)
  }
 
  window.requestAnimationFrame(main)
