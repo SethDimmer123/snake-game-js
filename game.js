fce1939d5167207562a337cca27f4595d91e100f
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-01 11:48:46 -0400

    initial commit

--- game.js
+++ game.js
@@ -1,12 +1,13 @@
 let lastRenderTime = 0// default time of my last render
 
- function main(currentTime) {
+const SNAKE_SPEED = 2 // this will be how many times the snake moves per second
+
+ function main(currentTime) {// I WILL NOT DO ANYTHING IN MY MAIN FUNCTION UNLESS I HIT A CERTAIN THRESHOLD
 
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    // number of seconds since last rendered divided by 1000 milliseconds
-    lastRenderTime = currentTime //updates my last render time with my current time.
-    console.log(currentTime)
+    lastRenderTime = currentTime 
+    console.log(secondsSinceLastRender)
  }
 
  window.requestAnimationFrame(main)
