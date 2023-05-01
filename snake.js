c4c45287c6d05e69eb6d24834264e1e2940cc105
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-01 12:28:40 -0400

    initial commit

--- snake.js
+++ snake.js
@@ -1,11 +1,22 @@
 // all the code for my snake goes
- export const SNAKE_SPEED = 1 // i am exporting the snake__speed so i can use it inside of game.js
+ export const SNAKE_SPEED = 1 
 
-export function update() { // i am exporting the update() function so i can use it inside of game.js
+ const snakeBody = [{ x:11, y:11 }]//drawing my snake in the very center of the screen at the very start
+ //snakeBody represents my snake
+// my snake is essentially in a bunch of different segments
+// the segments all are in a particular x and y position on the grid.
+
+ //css grid allows me to represent my snake as an x and y position
+
+export function update() { 
     console.log('update snake');
 }
 
-export function draw() { //i am exporting the draw() function so i can use it inside of game.js
-console.log('draw snake');
+export function draw(gameboard) { 
+    snakeBody.forEach(segments => {
+    //forEach lets me loop through the different pieces of the snake(segments)
+    //each piece of the snake i want to loop through
+    // i need t draw it to my gameboard so i can add my snake to the gameBoard 
+
+    })
 }
-//console logs get rendeered based on the snake spped is set 1 times quicker
