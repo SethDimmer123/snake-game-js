d9d499b6fc8f7fd66ea4afd7f751a3c4d99fb8fb
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-01 12:13:27 -0400

    initial commit

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
 // render code setup
 
+import { SNAKE_SPEED,update as updateSnake,draw as drawSnake, update } from "./snake.js"
+// to make sure everything is working check console log to see if 'render' is in console.
+
 let lastRenderTime = 0
 
-const SNAKE_SPEED = 2 
+// const SNAKE_SPEED = 2 
 
  function main(currentTime) {
 
@@ -16,15 +19,17 @@ const SNAKE_SPEED = 2
 
     //logic for my game 
 
-    Update() // this update loop will update all of the logic for my game.
-    //the update loop will move the snake to the correct position but not draw it 
-    // update loop will update if i ate the food or not and make the snake longer or shorter
-    //update loop will determine if i lost the game because i ran into my own tail
-    //  or ran into the edge of the screen.
+    Update()
 
-    draw() // the draw loop will draw everything on the screen based on my update loop.
-    // with the draw loop i will take all of my logic from update loop and say where is my snake now 
-    // lets draw the snake in the correct POSITION as well as draw the food where it needs to go 
+    draw()
  }
 
  window.requestAnimationFrame(main)
+
+ function Update() {//updating snake and food
+    updateSnake()// calling this function in my main update function
+ }
+
+ function draw() {// drawing snake and food
+
+ }
---
+++ snake.js
@@ -0,0 +1,10 @@
+// all the code for my snake goes
+ export const SNAKE_SPEED = 2 // i am exporting the snake__speed so i can use it inside of game.js
+
+export function update() { // i am exporting the update() function so i can use it inside of game.js
+
+}
+
+export function draw() { //i am exporting the draw() function so i can use it inside of game.js
+
+}
