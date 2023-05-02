0c16c73c405eb671358b1e8e32b2d825b10a43ed
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-02 11:32:15 -0400

    initial commit

--- food.js
+++ food.js
@@ -1,12 +1,14 @@
+import {onSnake,expandSnake} from './snake.js'
+
 let food = {x:10, y:1}// css grid starts at 1 
-const EXPANSION_RATE =  1 //how much the snake grows AFTER EATING FOOD.
-// 1 new segment after eating food.
+const EXPANSION_RATE =  1 
+
 
 
 export function update() { 
 if(onSnake(food)) {//
     expandSnake(EXPANSION_RATE)//EXPANDING SNAKE BY 1 
-    food ={x:20, y :10}
+    food ={x:20, y :10}//hard coding the food
 
 }
 }
--- snake.js
+++ snake.js
@@ -3,9 +3,8 @@ import { getInputDirection } from "./input.js"
 // all the code for my snake goes
  export const SNAKE_SPEED = 5
 
- const snakeBody = [
-    { x:11, y:11 },
-]
+ const snakeBody = [{ x:11, y:11 }]
+ let newSegments = 0 //defaulting to zero because my snake is not growing without eating food.
 
 
 
@@ -35,4 +34,24 @@ export function draw(gameBoard) {
     gameBoard.appendChild(snakeElement)
 
     })
+}
+
+export function expandSnake(amount) {
+    newSegments += amount 
+}
+
+export function onSnake(position) { // takes in a position and determines if this position is on my snake
+    return snakeBody.some(segment => {// IF my equal positions returns true for any of the snake positions(definition of SOME method) 
+        // for each segment i am checking if the segment is on my snake 
+        // so i compare my position with my segment position to see if they are equal.
+        return equalPositions(segment, position)
+    })
+    // if any part of my snakeBody equals the position i pass in then onSnake will return true. 
+}
+
+function equalPositions(pos1,pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+    //if my 2 positions are exactly the same 
+    // then the equalPositions will return TRUE 
+    
 }
