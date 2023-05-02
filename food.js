df6b6efb187f046eb3b33d3420f15ee2a5caa07e
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-02 11:16:51 -0400

    initial commit

--- food.js
+++ food.js
@@ -1,7 +1,14 @@
-let food = {x:0, y:0 }
+let food = {x:10, y:1}// css grid starts at 1 
+const EXPANSION_RATE =  1 //how much the snake grows AFTER EATING FOOD.
+// 1 new segment after eating food.
+
 
 export function update() { 
+if(onSnake(food)) {//
+    expandSnake(EXPANSION_RATE)//EXPANDING SNAKE BY 1 
+    food ={x:20, y :10}
 
+}
 }
 
 export function draw(gameBoard) { 
